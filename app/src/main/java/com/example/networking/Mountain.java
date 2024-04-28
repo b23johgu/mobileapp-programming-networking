23d15f9fd39c50d4e752c70f419f990923c1013e
gustafssonjohanna <johannakristina96@hotmail.com>
2024-04-28 18:29:45 +0200

    updated mountain class

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,37 +1,46 @@
 package com.example.networking;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Mountain {
     //Member variables
-    private String name;
-    private String location;
-    private int height;
+        private String ID;
+        private String name;
+        private String location;
+        @SerializedName("cost")
+        private int feet;
+        @SerializedName("size")
+        private int meter;
 
     //Constructor(s)
     public Mountain(String inName, String inLocation, int inHeight) {
         name = inName;
         this.location = inLocation;
-        this.height = inHeight;
+        this.meter = inHeight;
     }
 
     public Mountain(String inName){
         name=inName;
         location="";
-        height=-1;
+        meter=-1;
     }
 
     //Member methods
-    public String toString() { return name; }
+    public String toString() {
+        return name;
+    }
 
     public String info(){
         String str=name;
         str+=" is located in ";
         str+=location;
         str+=" and has a height of ";
-        str+=Integer.toString(height);
+        str+=Integer.toString(meter);
         str+="m";
         return str;
     }
     public void setHeight(int newHeight){
-        height = newHeight;
+        meter = newHeight;
     }
+
 }
