fb371d280e05393c75eaf8f8dc4d662e79cf9db0
gustafssonjohanna <johannakristina96@hotmail.com>
2024-04-28 21:31:56 +0200

    trying to implement json parsing

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -7,10 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import com.example.networking.RecyclerViewAdapter;
+import java.util.List;
+import java.util.Scanner;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -18,13 +22,24 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String mountains) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(mountains), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+
+            return null;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ArrayList<Mountain> items = new ArrayList<>(Arrays.asList(
-                new Mountain("Matterhorn"),
+                new Mountain("Matterhorn" , "USA" , 1000),
                 new Mountain("Mont Blanc"),
                 new Mountain("Denali")
         ));
@@ -40,12 +55,18 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
 
+        String s = readFile("mountains.json");
+        Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
+
         new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
+
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
     }
 
 }
