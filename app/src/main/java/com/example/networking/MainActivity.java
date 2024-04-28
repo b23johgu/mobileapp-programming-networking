bf83f371c5e244ff5e85d6dc8ed247d75ad6d38a
gustafssonjohanna <johannakristina96@hotmail.com>
2024-04-28 18:37:00 +0200

    created adapter

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.networking;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import com.example.networking.RecyclerViewAdapter;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                 new Mountain("Mont Blanc"),
                 new Mountain("Denali")
         ));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Mountain item) {
+                Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         Log.d("MountainItems", String.valueOf(items));
 
 
