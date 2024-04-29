56a948d9c691e8621d33da1ccc62a2b1f952a995
gustafssonjohanna <143899871+gustafssonjohanna@users.noreply.github.com>
2024-04-29 11:09:52 +0200

    changed variable name for mountain size

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -10,19 +10,20 @@ public class Mountain {
         @SerializedName("cost")
         private int feet;
         @SerializedName("size")
-        private int meter;
+        private int size;
+        private Auxdata auxdata;
 
     //Constructor(s)
     public Mountain(String inName, String inLocation, int inHeight) {
         name = inName;
         this.location = inLocation;
-        this.meter = inHeight;
+        this.size = inHeight;
     }
 
     public Mountain(String inName){
         name=inName;
         location="";
-        meter=-1;
+        size=-1;
     }
 
     //Member methods
@@ -35,12 +36,25 @@ public class Mountain {
         str+=" is located in ";
         str+=location;
         str+=" and has a height of ";
-        str+=Integer.toString(meter);
+        str+=Integer.toString(size);
         str+="m";
         return str;
     }
     public void setHeight(int newHeight){
-        meter = newHeight;
+        size = newHeight;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Auxdata getAuxdata() {
+        return auxdata;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public Integer getSize(){
+        return size;
+    }
 }
